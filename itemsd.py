860d0e438df7cdd5ba1982946e2bde95132851ab
takayu k <thiscall.rd@gmail.com>
2012-02-18 20:21:09 +0900

    modified itemsd.py

--- itemsd.py
+++ itemsd.py
@@ -6,6 +6,10 @@ import sys, os
 import json
 import re
 import glob
+import time
+import argparse
+
+from datetime import datetime
 from pit import Pit
 
 from Corpus import Corpus
@@ -15,63 +19,44 @@ import tweepy
 
 
 def logging(message):
-    
+    """ Given log-message, logging to specified output stream.
+    """
     sys.stdout.write(message + " %s\n" % datetime.now())
 
 
-def userfriends_fromname(api, db, coll_name, screen_name):
-
-    friends = set([ g.screen_name for g in tweepy.Cursor(api.friends, id=screen_name).items() ])
-
-    time_now = datetime.now()
-    stamp = int( time.mktime( time_now.timetuple() ) )
-    item = { "screen_name": screen_name, "friends": list(friends), "updated_at": stamp }
-    db.append(item)
-
-
 def activate_api(clientid=3):
-
+    """ Given client-id, activate Twitter REST API corresponds client-id.
+    """
     apikeys = Pit.get("twitter_restapi_%02d" % clientid)
     
     # create OAuth handler
-    consumer_key, consumer_sec = apikeys["consumer_key"], apikeys["consumer_secret"]
-    auth = tweepy.OAuthHandler(consumer_key, consumer_sec)
+    cons_key, cons_sec = apikeys["consumer_key"], apikeys["consumer_secret"]
+    auth = tweepy.OAuthHandler(cons_key, cons_sec)
 
     # set access token to OAuth handler
     token, token_sec = apikeys["access_token"], apikeys["access_token_secret"]
     auth.set_access_token(token, token_sec)
 
     # create API
-    proxyinfo = Pit.get("proxy")
-    api = tweepy.API(auth_handler=auth, proxy_host=proxyinfo["http"], proxy_port=proxyinfo["port"])
+    proxy = Pit.get("proxy")
+    url, port = proxy["http"], proxy["port"]
+    
+    api = tweepy.API(auth_handler=auth, proxy_host=url, proxy_port=port)
     return api
 
 
-def parse_args():
+def response_to_item(response):
 
-    usage = "[-f] [path-to-token] [-c] [collection name] [-l] [path-to-log]"
-   
-    parser = argparse.ArgumentParser(description="sanal.d")
+    item = {}
+    item["text"] = response.text
+    item["id"] = int(response.id_str)
+
+    item["created_at"] = int(time.mktime( response.created_at.timetuple() ))
     
-    parser.add_argument("-c", "--collname", required=True)
-    parser.add_argument("-u", "--screen_name", required=True)
-    parser.add_argument("-l", "--log", required=True)
+    item["screen_name"] = response.author.screen_name
 
-    args = parser.parse_args()
-    return args
-
-
-def extract(line):
-    
-    try:
-        item = json.loads(line)
-    except ValueError:
-        return None
-    
-    rawitem = json.loads(item["raw"])
-    if rawitem["user"]["lang"] == u"ja":
-        return (rawitem["user"]["screen_name"], rawitem["id"], rawitem["created_at"], re.sub("\n", "。", rawitem["text"]))
-
+    return item
+        
 
 def friends_of(user, api):
 
@@ -79,43 +64,56 @@ def friends_of(user, api):
         in tweepy.Cursor(api.friends, id=user).items() ])
 
 
-def pubitems(db, path):
+def useritems(db, screen_name):
 
-    publicitems = []
+    try:
+        latestitem = [ x for x
+                in users_db.findsorted(query={ "screen_name": screen_name }, sortkey="id") ][0]
+        since_id = latestitem["id"]
 
-    with file(path) as opened:
-        items = [ extract(line) for line in opened ]
-        publicitems += [ item for item in items if item ]
+        for res in api.user_timeline(id=screen_name, since_id=since_id):
+            db.append(response_to_item(res))
+
+    except IndexError:
+        for res in api.user_timeline(id=screen_name, count=50):
+            db.append(response_to_item(res))
+
+
+def parse_args():
+
+    usage = "[--interval] [interval] [-l] [path-to-log]"
    
-    return publicitems
+    parser = argparse.ArgumentParser(description="says")
+    parser.add_argument("--interval", type=int, default=1)
+    parser.add_argument("-l", "--log", default=".log/log")
 
-
-def useritems(db, path):
-
-    users = users_db.find({})
-    for u in users:
-        for v in friends_of(u, api):
-            pass
-        
-        
-
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
 
-    dbname, collname = sys.argv[1], sys.argv[2]
-    db = Corpus(database=dbname, collection=collname)
-   
-    #basedir = sys.argv[3]
+    args = parse_args()
+
+    dbinfo = Pit.get("says")
+
+    users_db = Corpus(database=dbinfo["db"], collection=dbinfo["users"])
+    users = users_db.find({})
 
     api = activate_api()
-    exit()
-   
-    for path in glob.glob("%s/*" % basedir):
-        publicitems = pubitems(db, path)
 
-        for item in publicitems:
-            itemd = dict(zip( ["screen_name", "id", "created_at", "text"], item ))
-            db.append(itemd)
+    items_db = Corpus(database=dbinfo["db"], collection=dbinfo["items"])
+    for u in users:
+        for v in friends_of(u["screen_name"], api):
+            try:
+                useritems(items_db, v)
+            except tweepy.error.TweepError:
+                continue
+            
+            logging("%s updated" % v)
 
-        print(path)
+            req_remain = api.rate_limit_status()["remaining_hits"]
+            logging("API request limit: %d" % req_remain)
+        
+            time.sleep(args.interval)
+
